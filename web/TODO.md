b9c82014471bad902f33b91920fd4efd13689ed7
Abe Fehr <abe.fehr@gmail.com>
2017-03-05 03:29:06 -0500

    A bunch more stuff

--- web/TODO.md
+++ web/TODO.md
@@ -1,10 +1,8 @@
 TODO
 ====
 
-- Start the React Native app
-- Finish putting the website online
-- Make a way to edit employees
-- Display which things are active in the admin section of the site
-- Make the favicon have a transparent background
-- Make a list of employees somewheres
-- Add the price boxes to the website
+- Make all employees have fake hours
+- Show something more useful on the dashboard, or at least clean it up
+- Make the data on the reports better
+- Make the data on the spreadsheets better
+- Clean up the extraneous settings in the admin page
--- web/database.js
+++ web/database.js
@@ -3,8 +3,93 @@ var createDatabase = function () {
   var _db = {
     employees: [
       {
-        id: 11817541,
-        name: 'Jonathan Toews',
+        id: 11823405,
+        first_name: 'Jonathan',
+        last_name: 'Toews',
+        wage: 23.50,
+        shifts: [
+          {
+            in: 1488665550,
+            out: 1488672954,
+          },
+        ],
+      },
+      {
+        id: 11823000,
+        first_name: 'Alec',
+        last_name: 'Baldwin',
+        wage: 23.50,
+        shifts: [
+          {
+            in: 1488665550,
+            out: 1488672954,
+          },
+        ],
+      },
+      {
+        id: 11823339,
+        first_name: 'Timothy',
+        last_name: 'Schroeder',
+        wage: 23.50,
+        shifts: [
+          {
+            in: 1488665550,
+            out: 1488672954,
+          },
+        ],
+      },
+      {
+        id: 11823993,
+        first_name: 'Miriam',
+        last_name: 'Knott',
+        wage: 23.50,
+        shifts: [
+          {
+            in: 1488665550,
+            out: 1488672954,
+          },
+        ],
+      },
+      {
+        id: 11821111,
+        first_name: 'Alicia',
+        last_name: 'Keys',
+        wage: 23.50,
+        shifts: [
+          {
+            in: 1488665550,
+            out: 1488672954,
+          },
+        ],
+      },
+      {
+        id: 11821232,
+        first_name: 'Victor',
+        last_name: 'Johnson',
+        wage: 23.50,
+        shifts: [
+          {
+            in: 1488665550,
+            out: 1488672954,
+          },
+        ],
+      },
+      {
+        id: 11829381,
+        first_name: 'Emily',
+        last_name: 'Groulx',
+        wage: 23.50,
+        shifts: [
+          {
+            in: 1488665550,
+            out: 1488672954,
+          },
+        ],
+      },
+      {
+        id: 11723455,
+        first_name: 'Tina',
+        last_name: 'Fey',
         wage: 23.50,
         shifts: [
           {
@@ -31,11 +116,20 @@ var createDatabase = function () {
       }
     },
     updateEmployee: function (data) {
+      var found = false;
       for (let i = 0; i < _db.employees.length; ++i) {
         if (_db.employees[i].id == data.id) {
-          _db.employees[i].name = data.name;
+          found = true;
+          _db.employees[i].first_name = data.first_name;
+          _db.employees[i].last_name = data.last_name;
           _db.employees[i].wage = data.wage;
         }
+      }
+      if (!found) {
+        console.log('THE DATA: ', data);
+        _db.employees.push(Object.assign({
+          shifts: [],
+        }, data));
       }
     },
   };
